4d687fc717a8ddb9c5bf1f5bcaac864d4f57862c
spvvrv <s.pivovarova@rambler.ru>
2023-02-09 17:24:19 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const bracketsDictionary = Object.fromEntries(bracketsConfig);
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let currentChar = str[i];
+    let lastElInStack = stack[stack.length - 1];
+
+    if (
+      currentChar in bracketsDictionary &&
+      currentChar !== bracketsDictionary[lastElInStack]
+    ) {
+      stack.push(currentChar);
+    } else {
+      if (currentChar !== bracketsDictionary[stack.pop()]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
